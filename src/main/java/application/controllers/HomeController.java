d33cf2fadeaba8b58b959ce7b93359fc64a8365b
Ricardo Santos <ricardosds@gmail.com>
2021-11-22 23:13:20 +0000

    adicionando comentarios no HomeController.java

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;   //import controlador de tipo
 import org.springframework.web.bind.annotation.RequestMapping;  //import do RequestMapping, é usado para mapear solicitações da web em métodos manipuladores em classes de manipulação de solicitações.
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller //A anotação @Controller indica que a classe é um “Controller” controlador, atuando como um estereótipo para a classe anotada, indicando o seu papel (dentro do contexto do padrão MVC ele é o 'C').
 public class HomeController {
     @RequestMapping(method=RequestMethod.GET)
     public String index() {
-        return "home/index.jsp";
+        return "home/index.jsp";    //retorna o arquivo index.jsp que se encontra na pasta home.
     }
 }
