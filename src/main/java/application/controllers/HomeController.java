8f45dd87fc0b7b85374fe4bf1b88cb8d573c9c86
Ricardo Santos <ricardosds@gmail.com>
2021-11-23 12:25:41 +0000

    adicionado comentarios no main/java/application/controllers/LivroController.java

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -1,15 +1,15 @@
 package application.controllers;    // Pacote application e indica em qual pacote este controllers (Controlador) estará localizado.
 
-import org.springframework.stereotype.Controller;   //import controlador de tipo de anotação, normalmente usado em combinação com métodos de manipulador anotados com base na RequestMapping. 
-import org.springframework.web.bind.annotation.RequestMapping;  //import do RequestMapping, é usado para mapear solicitações da web em métodos manipuladores em classes de manipulação de solicitações.
+import org.springframework.stereotype.Controller;   // Importa o pacote de Mapeamento de Requisições do Framework Spring.
+import org.springframework.web.bind.annotation.RequestMapping;  // Importa do RequestMapping, é usado para mapear solicitações da web em métodos manipuladores em classes de manipulação de solicitações.
 import org.springframework.web.bind.annotation.RequestMethod;   // Importa o pacote de Métodos de Requisição (GET, POST, DELETE, PUT, ETC) do Framework Spring.
 
-@Controller //A anotação @Controller indica que a classe é um “Controller” controlador, atuando como um estereótipo para a classe anotada, indicando o seu papel (dentro do contexto do padrão MVC ele é o 'C').
+@Controller // A anotação @Controller indica que a classe é um “Controller” controlador, atuando como um estereótipo para a classe anotada, indicando o seu papel (dentro do contexto do padrão MVC ele é o 'C').
 public class HomeController {   // Cria a classe pública HomeController e abre o escopo de corpo dela.
     @RequestMapping(method=RequestMethod.GET)   // Anotação para mapear solicitações da web em métodos em classes de tratamento de solicitações com assinaturas de método flexíveis.
 
     public String index() { // Cria uma função de retorno público de tipo String com o nome index, sem parâmetros.
 
-        return "home/index.jsp";    //Indica o retorno do arquivo "index.jsp" que está na pasta "home".
-    }   //Fecha o método index().
-}   //Fecha a classe HomeController.
+        return "home/index.jsp";    // Indica o retorno do arquivo "index.jsp" que está na pasta "home".
+    }   // Fecha o método index().
+}   // Fecha a classe HomeController.
--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -1,63 +1,65 @@
-package application.controllers;
+package application.controllers;    // Pacote application e indica em qual pacote este controllers (Controlador) estará localizado.
+
+import java.util.Optional;  // Improta um pacote opcional do java.util (Que possui muitas utilidades).
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller; // Importa o pacote de Mapeamento de Requisições do Framework Spring.
+import org.springframework.web.bind.annotation.RequestMapping;  //import do RequestMapping, é usado para mapear solicitações da web em métodos manipuladores em classes de manipulação de solicitações.
+import org.springframework.web.bind.annotation.RequestMethod; // Importa o pacote de Métodos de Requisição (GET, POST, DELETE, PUT, ETC) do Framework Spring.
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.Optional;
-import application.models.Livro;
-import application.repositories.LivroRepository;
 
-import org.springframework.ui.Model;
+import application.models.Livro;  // Importa o modelo Livro da aplicação.
+import application.repositories.LivroRepository;  // Importa o repositório LivroRepository da aplicação.
+import org.springframework.ui.Model;  // Importa um kit de ui do framework spring
 
-@Controller //A anotação @Controller indica que a classe é um “Controller” controlador, atuando como um estereótipo para a classe anotada, indicando o seu papel (dentro do contexto do padrão MVC ele é o 'C').
+@Controller // A anotação @Controller indica que a classe é um “Controller” controlador, atuando como um estereótipo para a classe anotada, indicando o seu papel (dentro do contexto do padrão MVC ele é o 'C').
 @RequestMapping("/livro") //A anotação @RequestMapping indica que o controlador, que neste caso é a classe "LivroController", receberá as requisições feitas à URL www.DNS_da_aplicação/LivroController.
-public class LivroController {  //
-  @Autowired  //O Autowired (@Autowired) é a anotação mais utiliza com relação a injeção de dependências. Como o próprio nome diz, o Autowired, indica um ponto aonde a injeção automática deve ser aplicada.
+public class LivroController {  // Cria a classe pública LivroController e abre o escopo de corpo dela.
+  @Autowired  // O Autowired (@Autowired) é a anotação mais utiliza com relação a injeção de dependências. Como o próprio nome diz, o Autowired, indica um ponto aonde a injeção automática deve ser aplicada.
   private LivroRepository livrosRepo; //Cria um objeto privado chamado livrosRepo do tipo LivroRepository.
 
-  @RequestMapping("/list")  //O método "list" também está anotado com a anotação @RequestMapping, indicando que esse método irá receber as requisições feitas a URL www.DNS_da_aplicação/cliente sempre que se tratar de uma requisição HTTP GET.
-  public String list(Model model) {
-    model.addAttribute("livros", livrosRepo.findAll());
-    return "list.jsp";  //o return serve para retornar algo de dentro do método que nesse caso, retorna "list.jsp".
+  @RequestMapping("/list")  //O método "list" também está anotado com a anotação @RequestMapping, indicando que esse método irá receber as requisições feitas a URL www.DNS_da_aplicação/LvroController sempre que se tratar de uma requisição HTTP GET.
+  public String list(Model model) { // Cria uma função de retorno público de tipo String com o nome list, com o parâmetro model do tipo Model, que serve para listar todos livros.
+    model.addAttribute("livros", livrosRepo.findAll()); // Adiciona o atributo livros ao modelo e preenche com todos livros do livrosRepo.
+    return "list.jsp";  // Indica o retorno do arquivo "list.jsp", Retorna uma string referente à página que mostrará os dados obtidos.
   }
 
-  public String listar() {
-    return "livro/list.jsp";
+  public String listar() {  // Cria uma função de retorno público de tipo String com o nome listar, sem parâmetros, que serve para listar todos livros dentro do "/livro".
+    return "livro/list.jsp";  //*********Retorna uma string referente à página que redirecionar ao método responsável pela listagem.
   }
 
-  @RequestMapping("/insert")
-  public String formInsert() {
-    return "insert.jsp";
+  @RequestMapping("/insert")  // Anotação para mapear solicitações da web em métodos em classes de tratamento de solicitações com assinaturas de método flexíveis.
+
+  public String formInsert() {  // Cria uma função de retorno público de tipo String com o nome listar, sem parâmetros, que serve para listar todos livros dentro do "/livro".
+    return "insert.jsp";  // Indica o retorno do arquivo "insert.jsp", Retorna uma string referente à página que mostrará os dados obtidos.
   }
 
   // inserindo os dados via formulário
-  @RequestMapping(value = "/insert", method = RequestMethod.POST)
-  public String saveInsert(@RequestParam("titulo") String titulo) {
-    Livro livro = new Livro();
-    livro.setTitulo(titulo);
-    livrosRepo.save(livro);
-    return "redirect:/livro/list";
+  @RequestMapping(value = "/insert", method = RequestMethod.POST) // Anotação para mapear solicitações da web em métodos em classes de tratamento de solicitações com assinaturas de método flexíveis, marca como essa função pode ser acessada pela web e seu método.
+  public String saveInsert(@RequestParam("titulo") String titulo) { // Cria uma função de retorno público de tipo String com o nome saveInsert, que recebe o título do livro como parâmetro, essa função serve para inserir títulos no repositório.
+    Livro livro = new Livro();  // Cria uma instância do objeto chamado livro do tipo Livro.
+    livro.setTitulo(titulo);  // Define o título do livro com o parâmetro recebido.
+    livrosRepo.save(livro); // Salva o livro no repositório.
+    return "redirect:/livro/list";  // Retorna um redirecionamento pra voltar para lista após a inserção.
   }
 
   // deletando os dados via formulário
-  @RequestMapping("/delete/{id}")
-  public String formDelete(Model model, @PathVariable int id) {
-    Optional<Livro> livro = livrosRepo.findById(id);
-    if (!livro.isPresent())
-      return "redirect:/livro/list";
-    model.addAttribute("livro", livro.get());
+  @RequestMapping("/delete/{id}") // Anotação para mapear solicitações da web em métodos em classes de tratamento de solicitações com assinaturas de método flexíveis, marca como essa função pode ser acessada pela web.
+  public String formDelete(Model model, @PathVariable int id) { // Cria uma função de retorno público de tipo String com o nome formDelete, que recebe o id do livro como parâmetro, essa função serve para confirmar se é possível remover títulos do repositório.
+    Optional<Livro> livro = livrosRepo.findById(id);  // Busca o livro pelo id e marca campo como opcional pra caso o id não seja encontrado.
+    if (!livro.isPresent()) // Define se o livro não for presente, o usuário será redirecionado de volta pra página que está.
+      return "redirect:/livro/list"; // Retorna um redirecionamento para voltar pra lista após a exclusão.
+    model.addAttribute("livro", livro.get()); // Adiciona o atributo livro ao model e preenche com o livro obtido.
 
-    return "/livro/delete.jsp";
+    return "/livro/delete.jsp"; //********Redireciona para a página de exclusão.
 
   }
 
-  @RequestMapping(value = "/delete", method = RequestMethod.POST)
-  public String confirmDelete(@RequestParam("id") int id) {
-    livrosRepo.deleteById(id);
-    return "redirect:/livro/list";
+  @RequestMapping(value = "/delete", method = RequestMethod.POST) // Anotação para mapear solicitações da web em métodos em classes de tratamento de solicitações com assinaturas de método flexíveis, marca como essa função pode ser acessada pela web e seu método.
+  public String confirmDelete(@RequestParam("id") int id) { // Cria uma função de retorno público de tipo String com o nome confirmDelete, que recebe o id do livro como parâmetro, essa função serve para remover títulos do repositório.
+    livrosRepo.deleteById(id);  // Executa a função de remover o livro pelo id do repositório de livros.
+    return "redirect:/livro/list";  // Retorna um redirecionamento para voltar para a lista de livros.
 
   }
 
