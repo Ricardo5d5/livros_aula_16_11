2713ad42fdf7e2a8ea9390c2778ad88434d78cbe
Ricardo Santos <ricardosds@gmail.com>
2021-11-23 10:52:32 +0000

    adionado comentarios no main/java/application/Application.java

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,11 +1,11 @@
-package application;
+package application;    // Define o pacote da aplicação como application
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;  // Importa o utilitário SpringApplication do springframework.
+import org.springframework.boot.autoconfigure.SpringBootApplication;    // Importa SpringBootApplication o utilitário do springframework.
 
-@SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
+@SpringBootApplication  // Indica uma classe de configuração que declara um ou mais métodos @Bean e também dispara a configuração automática e a varredura de componente.
+public class Application {  // Cria uma classe pública nomeada Application (Aplicação prinipal)
+    public static void main(String[] args) {    // Cria um método estático sem retorno que define a inicialização da aplicação
         SpringApplication.run(Application.class, args); //SpringApplication.run() para se inicializar uma aplicação com o Spring Boot.
     }
 }
